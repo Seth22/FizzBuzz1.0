af764919c6de2475e39927f0c2581a43e3bf0903
Sheriff John Brown <41117493+Seth22@users.noreply.github.com>
2022-09-21 18:54:34 -0400

    The Fizzest Buzzer

---
+++ FizzBuzz2/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Input the number of integers to Fizz Buzz!");
+        Scanner input = new Scanner(System.in);
+        int y = Math.abs(input.nextInt());
+        int x = 1;
+        while(x<=y) {
+            if (x % 3 == 0 && x % 5 == 0) {
+                System.out.println("Fizz Buzz");
+                ++x;
+            } else if (x % 3 == 0) {
+                System.out.println("Fizz");
+                ++x;
+            } else if (x % 5 == 0) {
+                System.out.println("buzz");
+                ++x;
+            } else {
+                System.out.println(x);
+                ++x;
+            }
+        }
+    }
+}
